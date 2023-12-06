e6de7fc640f74dc5a1fa522711daed9496135b1b
CaffeQ <102230900+CaffeQ@users.noreply.github.com>
2023-12-06 17:47:12 +0100

    minikube connection, but failed login

--- src/main/java/com/example/journalaccountservice/JournalAccountServiceApplication.java
+++ src/main/java/com/example/journalaccountservice/JournalAccountServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JournalAccountServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("Env: MYSQL_ROOT_PASSWORD=" + System.getenv("MYSQL_ROOT_PASSWORD") +" MYSQL_USER="+System.getenv("MYSQL_USER") + " MYSQL_URL"+ System.getenv("MYSQL_URL") );
         SpringApplication.run(JournalAccountServiceApplication.class, args);
     }
 
