76321a6d5974141d8e5748f2bda8af469115f1a5
tattack99 <62747925+tattack99@users.noreply.github.com>
2023-12-31 15:34:29 +0100

    commented out contextLoads

--- src/test/java/com/example/journalaccountservice/JournalAccountServiceApplicationTests.java
+++ src/test/java/com/example/journalaccountservice/JournalAccountServiceApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application.properties")
+//@SpringBootTest
 class JournalAccountServiceApplicationTests {
 
     @Test
